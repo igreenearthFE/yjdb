eadb60c08c6a5d01edea8b2d0cb9f7a7ffaf3bb8
Lutw123 <32935187+Lutw123@users.noreply.github.com>
2018-01-31 23:28:11 +0800

    提交登录模块、注册模块、重置密码模块

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,31 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Login from '../modules/login/index'
+import Reset from '../modules/reset/index'
+import Regist from '../modules/regist/index'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
+      path: '/login',
+      name: 'Login',
+      component: Login
+    },
+    {
+      path: '/reset',
+      name: 'Reset',
+      component: Reset
+    },
+    {
+      path: '/regist',
+      name: 'Regist',
+      component: Regist
+    },
+    {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      redirect: '/login'
     }
   ]
 })
